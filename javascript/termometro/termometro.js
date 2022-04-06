76d779823c67049644be267038925994556bf088
VITOR AUGUSTO ALMEIDA DE MOURA <v.augusto.am@gmail.com>
2022-04-06 20:41:44 -0300

    commit on main

---
+++ javascript/termometro/termometro.js
@@ -0,0 +1,20 @@
+var temperatura = 36.5
+if(temperatura >= 38){
+    console.log('Você está com febre!')
+} else {
+    console.log('Você não está com febre!')
+}
+
+var temperatura = 38
+if(temperatura >= 38){
+    console.log('Você está com febre!')
+} else {
+    console.log('Você não está com febre!')
+}
+
+var temperatura = 99
+if(temperatura >= 38){
+    console.log('Você está com febre!')
+} else {
+    console.log('Você não está com febre!')
+}
