ed93986a8010cfc2d81548bd264a641e228bcfee
VITOR AUGUSTO ALMEIDA DE MOURA <v.augusto.am@gmail.com>
2022-04-05 22:13:12 -0300

    add javascript folder

---
+++ javascript/ex001/variaveis.js
@@ -0,0 +1,9 @@
+var nome1 = 'Vitor'
+var idade1 = 23
+
+console.log('O usuário de nome: ' + nome1 + ' tem ' + idade1 + ' anos.')
+
+nome1 = 'João'
+idade1 = 22
+
+console.log('O usuário de nome: ' + nome1 + ' tem ' + idade1 + ' anos.')
