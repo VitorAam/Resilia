e6a54e12026d5d9697da90dd19c28dd86d6b1fb1
VITOR AUGUSTO ALMEIDA DE MOURA <v.augusto.am@gmail.com>
2022-05-05 13:50:38 -0300

    att

---
+++ javascript/lampada/index.js
@@ -0,0 +1,12 @@
+var lampada = {
+    cor: "RGB",
+    getCor: function(){
+        return this.cor
+    },
+    setCor: function(cornova){
+        this.cor = cornova
+    }
+}
+
+
+console.log(lampada.cor)
