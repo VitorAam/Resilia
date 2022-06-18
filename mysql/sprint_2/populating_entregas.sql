e281ce1a6ee4c370a44e5379d241733569923c74
VITOR AUGUSTO ALMEIDA DE MOURA <v.augusto.am@gmail.com>
2022-06-18 16:14:58 -0300

    Update populating_entregas.sql

--- mysql/sprint_2/populating_entregas.sql
+++ mysql/sprint_2/populating_entregas.sql
@@ -10,19 +10,19 @@ INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda',
 
 INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 2, 'Pronto');
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 2, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 2, 'Chegando lá');
 
 -- Aluno 3
 
 INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 3, 'Ainda não está pronto');
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 3, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 3, 'Mais que pronto');
 
 -- Aluno 4
 
 INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 4, 'Pronto');
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 4, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 4, 'Mais que pronto');
 
 -- Aluno 5
 
@@ -34,29 +34,29 @@ INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda',
 
 INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 6, 'Ainda não está pronto');
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 6, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 6, 'Mais que pronto');
 
 -- Aluno 7
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 7, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 7, 'Chegando lá');
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 7, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 7, 'Mais que pronto');
 
 -- Aluno 8
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 8, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 8, 'Mais que pronto');
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 8, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 8, 'Pronto');
 
 -- Aluno 9
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 9, 'Pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 9, 'Mais que pronto');
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 9, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 9, 'Mais que pronto');
 
 -- Aluno 10
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 10, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 10, 'Chegando lá');
 
 INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 10, 'Ainda não está pronto');
 
@@ -74,6 +74,6 @@ INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda',
 
 -- Aluno 13
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 13, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 1, 13, 'Mais que pronto');
 
-INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 13, 'Ainda não está pronto');
+INSERT INTO entregas (link_entrega, modulo, aluno, conceito) VALUES ('dsadasda', 2, 13, 'Mais que pronto');
